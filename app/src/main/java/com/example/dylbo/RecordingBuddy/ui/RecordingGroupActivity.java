ce76fa82d0e2e54675106b31cd2c1e2b06c2effe
djboyle <45405818+djboyle@users.noreply.github.com>
2019-09-13 21:39:29 +1000

    Commit 13SEP2019 Fix of permissions stopping recordings.

--- app/src/main/java/com/example/dylbo/RecordingBuddy/ui/RecordingGroupActivity.java
+++ app/src/main/java/com/example/dylbo/RecordingBuddy/ui/RecordingGroupActivity.java
@@ -1,11 +1,15 @@
 package com.example.dylbo.RecordingBuddy.ui;
 
+import android.Manifest;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -35,13 +39,28 @@ public class RecordingGroupActivity extends AppCompatActivity
 
         private RecyclerView mRecGroupRV;
         private RecordingGroupAdapter mRecGroupAdapter;
+        // Requesting permission to RECORD_AUDIO
+        private boolean permissionToRecordAccepted = false;
+        private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+        private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            switch (requestCode) {
+                case REQUEST_RECORD_AUDIO_PERMISSION:
+                    permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    break;
+            }
+            if (!permissionToRecordAccepted) finish();
+        }
 
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_recording_groups);
-
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
             Log.d(TAG, "Activity");
 
 
